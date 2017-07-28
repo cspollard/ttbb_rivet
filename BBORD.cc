9602a34bb29ea0d3e5f3d91f0142d2fb3c314beb
Johannes Krause <johannes.krause1@mailbox.tu-dresden.de>
2017-07-28 15:27:24 +0200

    bug fix in pt10 case

--- BBORD.cc
+++ BBORD.cc
@@ -162,7 +162,7 @@ namespace Rivet {
   class BBORD_DR04_PT10_parton : public BBORD {
   public:
     BBORD_DR04_PT10_parton()
-      :BBORD("BBORD_DR04_PT15_parton",0.4,10,true)
+      :BBORD("BBORD_DR04_PT10_parton",0.4,10,true)
     {   }
   };
   DECLARE_RIVET_PLUGIN(BBORD_DR04_PT10_parton);
